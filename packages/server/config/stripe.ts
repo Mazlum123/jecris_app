abdae91de0ae715570d5f16489350f84e803231b
Mazlum123 <mazlum.morcicek.dev@gmail.com>
2025-02-13 20:21:56 +0100

    Tests Railway v2

--- packages/server/config/stripe.ts
+++ packages/server/config/stripe.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("🔍 STRIPE_SECRET_KEY:", process.env.STRIPE_SECRET_KEY ? "Loaded" : "Not Loaded");
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2025-01-27.acacia",
 });
